46c98631491455708defea5feb672d07153cb23b
AlexanderMedia <alexandermediacreation@gmail.com>
2023-07-16 19:29:41 -0500

    test

---
+++ app/page.jsx
@@ -0,0 +1,20 @@
+
+
+const Home = () => {
+    return (
+        <section className="w-full flex-center flex-col">
+            <h1 className="head_text text-center">
+                The Coffee Cabin Co.
+                <br className="max-md:hidden" />
+                <span className="orange_gradient text-center">
+                    Located in Pensacola, Florida.
+                </span>
+            </h1>
+            <p className="desc text-center">
+                Hi! We're Liz & Megan - Besties & Baristas
+            </p>
+        </section>
+    )
+}
+
+export default Home
